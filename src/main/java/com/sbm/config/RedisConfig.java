e1e712abe993ddbc5a1eb525b6678f160e5ca3f2
miller <13061602629@163.com>
2018-07-10 19:33:49 +0800

    Revert: 修改配置

--- src/main/java/com/sbm/config/RedisConfig.java
+++ src/main/java/com/sbm/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.sbm.config;
 
+import com.sbm.util.MyMultipartResolver;
 import com.sbm.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.multipart.MultipartResolver;
 import redis.clients.jedis.JedisPoolConfig;
 
 /**
@@ -155,5 +157,17 @@ public class RedisConfig {
         RedisUtil redisUtil = new RedisUtil();
         redisUtil.setRedisTemplate(redisTemplate);
         return redisUtil;
+    }
+
+    @Bean(name = "multipartResolver")
+    public MultipartResolver multipartResolver() {
+        MyMultipartResolver resolver = new MyMultipartResolver();
+        resolver.setDefaultEncoding("UTF-8");
+        //resolveLazily属性启用是为了推迟文件解析，以在在UploadAction中捕获文件大小异常
+        resolver.setResolveLazily(true);
+        resolver.setMaxInMemorySize(40960);
+        //上传文件大小 5M 5*1024*1024
+        resolver.setMaxUploadSize(5 * 1024 * 1024);
+        return resolver;
     }
 }
