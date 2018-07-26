a1d13758bca0d44a737d3fdd1f45badeb132e7b8
Double287162738 <287162738@qq.com>
2018-07-26 15:48:36 +0800

    main.html优化

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,2 +1,8 @@
 $(function () {
+    // $(".item1").mouseenter(function () {
+    //     $(".item1-title").show();
+    // });
+    // $(".item1").mouseleave(function () {
+    //     $(".item1-title").hide();
+    // });
 });
